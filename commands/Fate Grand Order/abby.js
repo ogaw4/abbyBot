7ee8d7085891990edf668f06158533964044b3d1
ogaw4 <ogawa.gabriel@gmail.com>
2018-08-17 10:24:29 -0300

    moving abby images to local files

--- commands/Fate Grand Order/abby.js
+++ commands/Fate Grand Order/abby.js
@@ -29,7 +29,7 @@ module.exports = class AbbyCommand extends Command {
           color: 0xe55fbe,
           description: `Submitted by ${user}.`,
           url: picobj.src,
-          image: {url: `${Constants.db}images/abby/${picobj.fname}`}
+          image: {url: `../../images/abby/${picobj.fname}`}
         }
         message.channel.send('', {embed});
     });
