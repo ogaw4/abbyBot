f8726a4f937d55b39a136bf981506075b5d8c487
ogaw4 <ogawa.gabriel@gmail.com>
2018-08-24 09:04:25 -0300

    hug

--- commands/Fate Grand Order/hug.js
+++ commands/Fate Grand Order/hug.js
@@ -15,7 +15,7 @@ module.exports = class HugCommand extends Command {
       color: 0xe55fbe,
       description: `Yaaaay!! (\\*≧∀≦\\*)`,
       url: "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=69950901",
-      image: {url: "attachment://image.png"}
+      image: {url: "attachment://hug.png"}
     }
     message.channel.send('', {embed, files: [{ attachment: `${Constants.db}images/abbyhug.png`, name: 'hug.png'}]});
   }
