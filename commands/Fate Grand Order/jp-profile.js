ea6e819ce66daec7821aaa5a02644589764ffa2c
ogaw4 <ogawa.gabriel@gmail.com>
2020-02-11 09:08:48 +0900

    revert send message

--- commands/Fate Grand Order/jp-profile.js
+++ commands/Fate Grand Order/jp-profile.js
@@ -29,7 +29,7 @@ module.exports = class FGOProfileCommand extends Command {
     Promise.all([this.main.db.get(`fgoProfile_${player}`), this.main.client.fetchUser(player)]).then((profile) => {
       if (profile[0]) {
         profile[0] = JSON.parse(profile[0]);
-        if (!profile.privacy || !args) message.channel.send(this.main.util.fgoProfile(profile[1], profile[0]));
+        if (!profile.privacy || !args) message.channel.send('', {embed: this.main.util.fgoProfile(profile[1], profile[0])});
         else message.channel.send(`No peeking! That player has their privacy set to true.`);
       } else if (args) message.channel.send(`Cannot find profile of provided player. Please check the mention and try again!`);
       else message.channel.send(`Profile not found, please use \`${prefix}jp-profile-edit\` to create one.`);
