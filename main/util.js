e6e3f39511f6612df9d998bcf70b4582b35d1b0a
ogaw4 <ogawa.gabriel@gmail.com>
2020-02-11 09:11:57 +0900

    fix typo

--- main/util.js
+++ main/util.js
@@ -55,7 +55,8 @@ module.exports = class Util {
       .setTitle("FGO Profile for " + user.username)
       .addBlankField()
       .addField("IGN:", data.name || "Not Provided", true)
-      .addField("Freind ID", data.id || "Not Provided", true)
+      .addBlankField()
+      .addField("Friend ID", data.id || "Not Provided", true)
       .setThumbnail(user.displayAvatarURL);
     
     if (data.support) {
